3e1f2d928c422ffc5b2bed035cca4e72d1dbd0ae
Artem Vasiliev <artem.job@gmail.com>
2016-06-21 19:06:12 +0300

    docs: a note re Gemnasium autoupdate script

--- gems-update.sh
+++ gems-update.sh
@@ -1,3 +1,4 @@
+# for this to work you need to set GEMNASIUM_TOKEN env var first
 export GEMNASIUM_TESTSUITE="true"
 export GEMNASIUM_PROJECT_SLUG="artemv/cbr-rates"
 gemnasium autoupdate run && gemnasium autoupdate apply && bundle exec rspec spec && git commit -m "chore(dependencies): gemnasium autoupdate" Gemfile.lock && git push
